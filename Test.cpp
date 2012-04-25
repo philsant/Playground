31cdf38864c12d5a5ba416c22404cdcd1f1709c0
Philip Sant <philip_sant@yahoo.co.uk>
2012-04-25 15:15:07 +0000

    Added Function and Inout

--- Test.cpp
+++ Test.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
+
+string Test(int i)
+{
+	stringstream  TestString; 
+	TestString <<  "The Number Passed Was " << i;
+	return TestString.str();
+}
+
 int main()
 {
+	int i;	
+
 	cout << "Hello World!" << endl;
 	cout << "Welcome to C++ Programming" << endl;
+	cout << "Testing out vi as an editor" << endl;
+	cout << "Please enter a number: ";
+	cin >> i;
+
+	string TestString; 
+	TestString = Test(i);
+	
+	cout << TestString << endl;
 	return 0;
 }
+
